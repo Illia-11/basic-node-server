a22f5edc367c5470338def56bc189125488e4d79
Illia Piddubnyi <illya2008zp@gmail.com>
2024-12-20 18:50:28 +0200

    Add CommonJS syntax examples

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-// іменний імпорт (ESModules)
-import { test, testFunc as newTestFuncName } from "./myMath.js";
+// // іменний імпорт (ESModules)
+// import { test, testFunc as newTestFuncName } from "./myMath.js";
 
-import * as MyMath from "./myMath.js";
+// import * as MyMath from "./myMath.js";
 
-// імпорт за замовчанням (ESModules)
-import MyMath from "./myMath.js";
+// іменний імпорт (CommonJS)
+const { test, testFunc: myTestFunc } = require("./myMath.js");
 
-console.log(MyMath);
+// // імпорт за замовчанням (ESModules)
+// import MyMathModule from "./myMath.js";
 
-newTestFuncName();
+// // імпорт за замовчанням (CommonJS)
+const myMathExports = require("./myMath.js");
+
+console.log(myMathExports);
+
+// console.log(MyMath);
+
+// newTestFuncName();
+
+// console.log(__dirname); // шлях до папки, що містить файл
+// console.log(__filename); // шлях до файлу, що запущено
+
+// console.log(module);
--- myMath.js
+++ myMath.js
@@ -2,16 +2,42 @@ class MyMath {
   static PI = 3.14;
 }
 
-// експорт за замовчанням (ESModules)
-export default MyMath;
+// // експорт за замовчанням (ESModules)
+// export default MyMath;
 
-// export default S; --- ERROR
+// // export default S; --- ERROR
+
+// експорт за замовчанням (CommonJS)
+
+module.exports = MyMath;
+
+// module.exports = 5; // не помилка, але перезапис
+
+// // іменний експорт (ESModules)
+// export { MyMath };
+
+// export const test = 5;
+
+// export function testFunc() {
+//   console.log("test func");
+// }
 
 // іменний експорт (ESModules)
-export { MyMath };
 
-export const test = 5;
+// якщо exports це обʼєкт
+module.exports.test = 5;
 
-export function testFunc() {
+module.exports.testFunc = () => {
   console.log("test func");
 }
+
+// якщо exports це не обʼєкт
+module.exports = {
+  test: 5,
+  MyMath,
+  testFunc : () => {
+    console.log("test func");
+  }
+}
+
+// console.log(module);
