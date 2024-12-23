c3c0c36e07f46fdacd150147edb9f3c31134471b
Illia Piddubnyi <illya2008zp@gmail.com>
2024-12-23 17:50:01 +0200

    Add basic node server logic for POST and GET routes

--- index.js
+++ index.js
@@ -1,19 +1,55 @@
-const http = require("http");
+const http = require('http');
 
-// функція, що запускається при надходженні запиту на сервер
+const users = [{ id: 0 }, { id: 1 }, { id: 2 }];
+
+// функція, що зпускається при надхоженні запиту на сервер
 function requestListener(request, response) {
-  console.log(request); // обʼєкт запита клієнта
-  console.log(response); // обʼєкт відповіді сервера
+  // console.log(request); // об'єкт запита клієнта
+  // console.log(response); // об'єкт відповіді сервера
 
-  // посилаємо відповідь клієнту, який робив запит
-  // має бути рядком і має бути обовʼязково присутнім
-  response.end("Thanks for visiting!");
+  // рядок методу з запиту
+  const method = request.method;
+  // рядок з шляхом на який було зроблшено запит у сервері (без хоста і порта)
+  const path = request.url;
+
+  if (method === 'GET') {
+    if (path === '/users') {
+      response.end(JSON.stringify(users));
+    } else if (path === '/comments') {
+      response.end(JSON.stringify([{ id: 0, text: 'bla bla bla' }]));
+    }
+  } else if (method === 'POST') {
+
+    if(path === '/users') {
+      let userJsonString = '';
+
+      request.on('data', (chunk) => {
+        userJsonString += chunk;
+      });
+
+      request.on('end', () => {
+        const user = JSON.parse(userJsonString);
+
+        // ...
+
+        user.id = Date.now();
+
+        users.push(user);
+
+        response.end('User registered');
+      });
+    }
+  }
+
+  // посилаємо відповідь кліенту який робив запит
+  // має бути рядком і має бути обов'язково присутнім
+  // response.end(`Thanks for visiting! Your method was ${request.method}`);
 }
 
-// створюємо екзеспляр сервера
+// створюємо екземпляр сервера
 const server = http.createServer(requestListener);
 
-console.log(server);
+// console.log(server);
 
 const PORT = 3000;
 
